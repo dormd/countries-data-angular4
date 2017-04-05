1564a21841df3e93069b6c567d8509825aec1007
Dor Moshe <dmoshe@interwise.com>
2017-04-05 20:57:17 +0300

    data access service & main component - support new country & anthem details

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -10,13 +10,18 @@ export class AppComponent implements OnInit {
   countriesA2s: string[];
   selectedA2 = 'IL';
 
+  nativeCountryName: string;
+  commonCountryName: string;
   capital: string;
-  continent: string;
-  region: string;
-  diallingNationalPrefix: string;
+  continents: string;
+  subRegion: string;
+  callingCode: string;
   area: number;
   populationCount: number;
   populationPrecentage: number;
+  languages: string;
+  wikiLinkSuffix: string;
+  anthemLink: string;
 
   constructor(private _dataAccessService: DataAccessService) { }
 
@@ -29,15 +34,17 @@ export class AppComponent implements OnInit {
   onA2Changed(a2) {
     this.selectedA2 = a2;
 
+    this.nativeCountryName = this._dataAccessService.getNativeCountryName(this.selectedA2);
+    this.commonCountryName = this._dataAccessService.getCommonCountryName(this.selectedA2);
     this.capital = this._dataAccessService.getCapital(this.selectedA2);
-    this.continent = this._dataAccessService.getContinent(this.selectedA2);
-    this.region = this._dataAccessService.getRegion(this.selectedA2);
-    this.diallingNationalPrefix = this._dataAccessService.getDiallingNationalPrefix(this.selectedA2);
+    this.continents = this._dataAccessService.getContinents(this.selectedA2);
+    this.subRegion = this._dataAccessService.getSubRegion(this.selectedA2);
+    this.callingCode = this._dataAccessService.getCallingCodePrefix(this.selectedA2);
     this.area = this._dataAccessService.getArea(this.selectedA2);
     this.populationCount = this._dataAccessService.getPopulationCount(this.selectedA2);
     this.populationPrecentage = this._dataAccessService.getPopulationPrecentage(this.selectedA2);
-  }
-
-  private _setupName() {
+    this.languages = this._dataAccessService.getLanguages(this.selectedA2);
+    this.anthemLink = this._dataAccessService.getAnthemLink(this.selectedA2);
+    this.wikiLinkSuffix = this._dataAccessService.getWikiSuffix(this.selectedA2);
   }
 }
--- src/app/services/data-access.service.ts
+++ src/app/services/data-access.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Countries, ICountry, Anthems, IAnthem } from 'countries-data';
+import { Countries, ICountry, Anthems, IAnthem, IOfficialCommon } from 'countries-data';
 import * as _ from 'lodash';
 
 import { COUNTRIES_DATA, ANTHEMS_DATA } from '../tokens/countries.tokens';
@@ -10,39 +10,69 @@ export class DataAccessService {
   constructor(@Inject(ANTHEMS_DATA) private _anthemsData: Anthems,
               @Inject(COUNTRIES_DATA) private _countriesData: Countries) { }
 
-  getCountriesA2s(): string[] {
-    return _.keys(this._countriesData);
+  getNativeCountryName(a2: string): string {
+    const values: IOfficialCommon[] = _.values(this.getCountryObjByA2(a2).name.native);
+    if (values.length > 0) {
+        return values[0].common;
+    }
+
+    return '';
+  }
+
+  getCommonCountryName(a2: string): string {
+    return this.getCountryObjByA2(a2).name.common;
   }
 
   getCapital(a2: string): string {
-    return this._countriesData[a2.toUpperCase()].capital;
+    return this.getCountryObjByA2(a2).capital;
   }
 
-  getContinent(a2: string): string {
-    return this.getInCommonLanguage(this._countriesData[a2.toUpperCase()].geo.continent);
+  getContinents(a2: string): string {
+    const continentObj = this.getCountryObjByA2(a2).geo.continent;
+
+    // for country over two continents
+    return _.values(continentObj).join(', ');
   }
 
-  getRegion(a2: string): string {
-    return this._countriesData[a2.toUpperCase()].geo.region;
+  getSubRegion(a2: string): string {
+    return this.getCountryObjByA2(a2).geo.subregion;
   }
 
-  getDiallingNationalPrefix(a2: string): string {
-    return this._countriesData[a2.toUpperCase()].dialling.national_prefix;
+  getCallingCodePrefix(a2: string): string {
+    const callingCode: string[] | number[] = this.getCountryObjByA2(a2).dialling.calling_code || [''];
+    return callingCode.join(', ');
   }
 
   getArea(a2: string): number {
-    return this._countriesData[a2.toUpperCase()].geo.area;
+    return this.getCountryObjByA2(a2).geo.area;
   }
 
   getPopulationCount(a2: string): number {
-    return this._countriesData[a2.toUpperCase()].population.count;
+    return this.getCountryObjByA2(a2).population.count;
   }
 
   getPopulationPrecentage(a2: string): number {
-    return this._countriesData[a2.toUpperCase()].population.worldPercentage;
+    return this.getCountryObjByA2(a2).population.worldPercentage;
   }
 
-  getInCommonLanguage(values): string {
-    return '';
+  getLanguages(a2: string): string {
+    return _.values(this.getCountryObjByA2(a2).languages).join(', ');
+  }
+
+  getAnthemLink(a2: string): string {
+    const anthemObj: IAnthem = this._anthemsData[a2];
+    return anthemObj ? this._anthemsData[a2].link : null;
+  }
+
+  getWikiSuffix(a2: string): string {
+    return this.getCountryObjByA2(a2).wikiLink; 
+  }
+
+  getCountriesA2s(): string[] {
+    return _.keys(this._countriesData).sort();
+  }
+
+  getCountryObjByA2(a2: string): ICountry {
+    return this._countriesData[a2.toUpperCase()];
   }
 }
